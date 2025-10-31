ed50d7509c40da898267401ced318baf8ff66f3f
MauroBorzi <mauroborzi91@gmail.com>
2025-10-31 10:27:11 +0100

    Move password generation logic to functions.php

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
 
-$password = "";
-
-if (isset($_GET["length"])) {
-
-  $lower = "abcdefghijklmnopqrstuvwxyz";
-  $upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  $numbers = "0123456789";
-  $symbols = "!@#$%^&*()-_=+[]{};:,.<>?/~`";
-
-  $allChar = $lower . $upper . $numbers . $symbols;
-
-  for ($i = 0; $i < $_GET["length"]; $i++) {
-
-    $randomPosition = rand(0, strlen($allChar) - 1);
-    $randomChar = substr($allChar, $randomPosition, 1);
-
-    $password .= $randomChar;
-  }
-}
+require_once "./functions.php";
 
 ?>
 
